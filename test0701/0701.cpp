e25272cbd3d1d72a365dce3acdab3d962af43adb
123wdc <wdc20040412@outlook.com>
2023-07-22 11:35:21 +0800

    dev

--- test0701/0701.cpp
+++ test0701/0701.cpp
@@ -246,186 +246,186 @@ struct book {
 //	return true;
 //}
 
-struct stack {
-	int* base;
-	int* top;
-	int stacksize;
-};
-#define STACK_INIT_SIZE 100
-#define STACKINCREMENT 10
-
-void InitStack(stack& S) {
-	//构造一个空栈
-	S.base = (int*)malloc(STACK_INIT_SIZE * sizeof(int));
-	if (S.base == NULL) {
-		exit(0);//存储分配失败
-	}
-	S.top = S.base;
-	S.stacksize = STACK_INIT_SIZE;
-}
-
-void Push(stack& S, int e) {
-	//插入元素e作为新的栈顶元素
-	if (S.top - S.base >= S.stacksize) {
-		S.base = (int*)realloc(S.base, (S.stacksize + STACKINCREMENT) * sizeof(int));//变S.base;
-		if (S.base == NULL) {
-			exit(0);
-		}
-		S.top = S.base + S.stacksize;//变S.top
-		S.stacksize = S.stacksize + STACK_INIT_SIZE;//变S.stacksize
-	}
-	*(S.top) = e;
-	S.top++;
-	//*S.top++ = e;
-}
-
-bool Pop(stack& S, int& e) {
-	//若栈不空，则删除S的栈顶元素，用e返回其值
-	if (S.base == S.top) {
-		return false;
-	}
-	S.top--;
-	e = *(S.top);
-	//e = *--S.top;
-	return true;
-}
-
-bool Pop(stack& S) {
-	//若栈不空，则删除S的栈顶元素
-	if (S.base == S.top) {
-		return false;
-	}
-	S.top--;
-	//e = *(S.top);
-	//e = *--S.top;
-	return true;
-}
-
-bool GetTop(stack& S, int& e) {
-	//若栈不空。则用e返回S的栈顶元素
-	if (S.top == S.base) {
-		return false;
-	}
-	e = *(S.top - 1);
-	return true;
-}
-bool StackEmpty(stack& S) {
-	if (S.base == S.top)return true;
-	else return false;
-}
-
-void ClearStack(stack& S, int& n) {
-	//清空栈，将数据放在n上调出
-	while (!StackEmpty(S)) {
-		n = 0;
-		Pop(S, n);
-	}
-}
-void DestoryStack(stack& S) {
-	//销毁栈
-	if (S.base != NULL) {
-		delete S.base;
-		S.base = S.top = NULL;
-		S.stacksize = 0;
-	}
-}
-
-void conversion(stack& S) {
-	InitStack(S);
-	int n1 = 1348;
-	//scanf_s("%d", &n1);
-	while (n1) {
-		Push(S, n1 % 8);
-		n1 = n1 / 8;
-	}
-	while (!StackEmpty(S)) {
-		int value = 0;
-		Pop(S, value);
-		printf("%d", value);
-	}
-}
-
-void check(stack& S, int n) {
-	InitStack(S);
-	int tem = 0;
-	if (StackEmpty(S)) {
-		Push(S, n);
-	}
-	else {
-		GetTop(S, tem);
-		if (tem == n) {
-			Pop(S);
-		}
-		else {
-			Push(S, n);
-		}
-	}
-}
-
-struct BookQueue
-{
-	book* front;//队头指针
-	book* rear;//队尾指针
-};
-
-void InitQueue(BookQueue& B) {
-	B.front = B.rear = (book*)malloc(sizeof(book));
-	if (B.front == NULL) {
-		exit(0);
-	}
-	B.front->next = NULL;
-}
-
-void DestoryQueue(BookQueue& B) {
-	while (B.front)
-		B.rear = B.front->next;
-	free(B.front);
-	B.front = B.rear;
-}
-
-void EnQueue(BookQueue& B, int e) {
-	book* p = (book*)malloc(sizeof(book));
-	if (p == NULL) {
-		exit(0);
-	}
-	p->price = e; p->next = NULL;
-	B.rear->next = p;
-	B.rear = p;
-}
-
-bool DeQueue(BookQueue& B, int& e) {
-	if (B.front == B.rear) { return false; }
-	book* p = B.front->next;
-	e = p->price;
-	B.front->next = p->next;
-	if (p == B.rear) { B.front = B.rear; }
-	free(p);
-	return true;
-}
-
-void ClearQueue(BookQueue& B) {
-	while (B.front != B.rear)
-	{
-		book* p = B.front->next;
-		//e = p->price;
-		B.front->next = p->next;
-		if (p == B.rear) { B.front = B.rear; }
-		free(p);
-	}
-}
-
-bool QueueEmpty(BookQueue& B) {
-	if (B.front == B.rear) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-void GetHead(BookQueue& B, int& e) {
-	e = B.front->next->price;
-}
+//struct stack {
+//	int* base;
+//	int* top;
+//	int stacksize;
+//};
+//#define STACK_INIT_SIZE 100
+//#define STACKINCREMENT 10
+//
+//void InitStack(stack& S) {
+//	//构造一个空栈
+//	S.base = (int*)malloc(STACK_INIT_SIZE * sizeof(int));
+//	if (S.base == NULL) {
+//		exit(0);//存储分配失败
+//	}
+//	S.top = S.base;
+//	S.stacksize = STACK_INIT_SIZE;
+//}
+//
+//void Push(stack& S, int e) {
+//	//插入元素e作为新的栈顶元素
+//	if (S.top - S.base >= S.stacksize) {
+//		S.base = (int*)realloc(S.base, (S.stacksize + STACKINCREMENT) * sizeof(int));//变S.base;
+//		if (S.base == NULL) {
+//			exit(0);
+//		}
+//		S.top = S.base + S.stacksize;//变S.top
+//		S.stacksize = S.stacksize + STACK_INIT_SIZE;//变S.stacksize
+//	}
+//	*(S.top) = e;
+//	S.top++;
+//	//*S.top++ = e;
+//}
+//
+//bool Pop(stack& S, int& e) {
+//	//若栈不空，则删除S的栈顶元素，用e返回其值
+//	if (S.base == S.top) {
+//		return false;
+//	}
+//	S.top--;
+//	e = *(S.top);
+//	//e = *--S.top;
+//	return true;
+//}
+//
+//bool Pop(stack& S) {
+//	//若栈不空，则删除S的栈顶元素
+//	if (S.base == S.top) {
+//		return false;
+//	}
+//	S.top--;
+//	//e = *(S.top);
+//	//e = *--S.top;
+//	return true;
+//}
+//
+//bool GetTop(stack& S, int& e) {
+//	//若栈不空。则用e返回S的栈顶元素
+//	if (S.top == S.base) {
+//		return false;
+//	}
+//	e = *(S.top - 1);
+//	return true;
+//}
+//bool StackEmpty(stack& S) {
+//	if (S.base == S.top)return true;
+//	else return false;
+//}
+//
+//void ClearStack(stack& S, int& n) {
+//	//清空栈，将数据放在n上调出
+//	while (!StackEmpty(S)) {
+//		n = 0;
+//		Pop(S, n);
+//	}
+//}
+//void DestoryStack(stack& S) {
+//	//销毁栈
+//	if (S.base != NULL) {
+//		delete S.base;
+//		S.base = S.top = NULL;
+//		S.stacksize = 0;
+//	}
+//}
+//
+//void conversion(stack& S) {
+//	InitStack(S);
+//	int n1 = 1348;
+//	//scanf_s("%d", &n1);
+//	while (n1) {
+//		Push(S, n1 % 8);
+//		n1 = n1 / 8;
+//	}
+//	while (!StackEmpty(S)) {
+//		int value = 0;
+//		Pop(S, value);
+//		printf("%d", value);
+//	}
+//}
+//
+//void check(stack& S, int n) {
+//	InitStack(S);
+//	int tem = 0;
+//	if (StackEmpty(S)) {
+//		Push(S, n);
+//	}
+//	else {
+//		GetTop(S, tem);
+//		if (tem == n) {
+//			Pop(S);
+//		}
+//		else {
+//			Push(S, n);
+//		}
+//	}
+//}
+//
+//struct BookQueue
+//{
+//	book* front;//队头指针
+//	book* rear;//队尾指针
+//};
+//
+//void InitQueue(BookQueue& B) {
+//	B.front = B.rear = (book*)malloc(sizeof(book));
+//	if (B.front == NULL) {
+//		exit(0);
+//	}
+//	B.front->next = NULL;
+//}
+//
+//void DestoryQueue(BookQueue& B) {
+//	while (B.front)
+//		B.rear = B.front->next;
+//	free(B.front);
+//	B.front = B.rear;
+//}
+//
+//void EnQueue(BookQueue& B, int e) {
+//	book* p = (book*)malloc(sizeof(book));
+//	if (p == NULL) {
+//		exit(0);
+//	}
+//	p->price = e; p->next = NULL;
+//	B.rear->next = p;
+//	B.rear = p;
+//}
+//
+//bool DeQueue(BookQueue& B, int& e) {
+//	if (B.front == B.rear) { return false; }
+//	book* p = B.front->next;
+//	e = p->price;
+//	B.front->next = p->next;
+//	if (p == B.rear) { B.front = B.rear; }
+//	free(p);
+//	return true;
+//}
+//
+//void ClearQueue(BookQueue& B) {
+//	while (B.front != B.rear)
+//	{
+//		book* p = B.front->next;
+//		//e = p->price;
+//		B.front->next = p->next;
+//		if (p == B.rear) { B.front = B.rear; }
+//		free(p);
+//	}
+//}
+//
+//bool QueueEmpty(BookQueue& B) {
+//	if (B.front == B.rear) {
+//		return true;
+//	}
+//	else {
+//		return false;
+//	}
+//}
+//
+//void GetHead(BookQueue& B, int& e) {
+//	e = B.front->next->price;
+//}
 
 inline int read() {
 	char c = getchar(); int x = 0, f = 1;
